984582a2f14a2918e7f938902ab02caf82933f2a
Mark Easterling <marke.easterling@gmail.com>
2016-09-08 09:49:13 -0500

    removed lookup fields and route options

--- server/music/views.py
+++ server/music/views.py
@@ -1,18 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from music.models import Artist, Album, Song
 from music.serializers import ArtistSerializer, AlbumSerializer, SongSerializer
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        "artist": reverse("artist-list", request=request, format=format),
-        "album": reverse("album-list", request=request, format=format),
-        "song": reverse("song-list", request=request, format=format)
-    })
 
 class ArtistList(viewsets.ModelViewSet):
     model = Artist
@@ -23,7 +12,6 @@ class ArtistDetail(viewsets.ModelViewSet):
     model = Artist
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    lookup_field = 'name'
 
 class AlbumList(viewsets.ModelViewSet):
     model = Album
@@ -34,7 +22,6 @@ class AlbumDetail(viewsets.ModelViewSet):
     model = Album
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    lookup_field = 'name'
 
 class SongList(viewsets.ModelViewSet):
     model = Song
@@ -45,4 +32,3 @@ class SongDetail(viewsets.ModelViewSet):
     model = Song
     queryset = Song.objects.all()
     serializer_class = SongSerializer
-    lookup_field = 'name'
