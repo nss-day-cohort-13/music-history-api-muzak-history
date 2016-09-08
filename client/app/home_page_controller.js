c63ed9e30258122d2da6e7dfbf81a8eae1f7c157
Mark Easterling <marke.easterling@gmail.com>
2016-09-08 16:51:15 -0500

    fixes for pagination addition

--- client/app/home_page_controller.js
+++ client/app/home_page_controller.js
@@ -1,25 +1,34 @@
 angular.module("app")
-    .controller("HomePageCtrl", function($http, RootFactory) {
+    .controller("HomePageCtrl", function($http, RootFactory, $timeout) {
       const home = this;
       home.songs = [];
       home.nextPageUrl = "";
+      home.artists = [];
+      home.albums = [];
+
 
       RootFactory.root
-        .then(data => $http.get(data.song))
-        .then(songsRes => home.songs = songsRes.data)
-        .then(() => home.songs.forEach(song =>
-          $http.get(song.url).then(res => Object.assign(song, res.data))
-          .then(s => console.log("s", s))
-        ))
+      .then(data => $http.get(data.artist))
+      .then(res => home.artists = res.data.results)
+      .then(s => console.log(s))
 
-        RootFactory.root
-          .then(data => $http.get(data.artist))
-          .then(res => home.artist = res.data)
-          .then(s => console.log(s))
+      RootFactory.root
+      .then(data => $http.get(data.album))
+      .then(res => home.albums = res.data.results)
+      .then(s => console.log(s))
 
-        RootFactory.root
-          .then(data => $http.get(data.album))
-          .then(res => home.album = res.data)
-          .then(s => console.log(s))
+      home.more = () => { if(home.nextPageUrl) loadSongs(home.nextPageUrl); }
+
+      function loadSongs(url) {
+        $http.get(url)
+          .then(songsRes => songsRes.data)
+          .then(songsData => { home.nextPageUrl = songsData.next; return songsData.results; })
+          .then(songList => songList.forEach(song =>
+            $http.get(song.url)
+              .then(res => {home.songs.push(res.data); return res.data})
+          ));
+      }
+
+      RootFactory.root.then(data => loadSongs(data.song));
 
     })
