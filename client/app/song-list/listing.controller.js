afc78a71f70debffe0fde4d78a88c479de2a3c8e
Adam Oswalt <email@adamoswalt.com>
2016-09-08 23:33:27 -0500

    mass reorganizeify

--- client/app/song-list/listing.controller.js
+++ client/app/song-list/listing.controller.js
@@ -1,51 +1,51 @@
 angular.module("app")
-    .controller("HomePageCtrl", function($http, $uibModal, RootFactory) {
-      const home = this;
-      home.songs = [];
-      home.artists = [];
-      home.albums = [];
-      home.nextPageUrl = "";
+    .controller("ListingCtrl", function($http, $uibModal, RootFactory) {
+      const listing = this;
+      listing.songs = [];
+      listing.artists = [];
+      listing.albums = [];
+      listing.nextPageUrl = "";
 
 
-      home.showSongInfo = song => $uibModal.open({
-        templateUrl: "app/_song-modal.html",
+      listing.showSongInfo = song => $uibModal.open({
+        templateUrl: "app/song-info/song-info-modal.html",
         controller: "SongInfoModalCtrl",
         controllerAs: "songInfo",
         resolve: { song }
       });
 
-      home.more = () => {
-        if(home.nextPageUrl) { loadSongs(home.nextPageUrl); }
+      listing.more = () => {
+        if(listing.nextPageUrl) { loadSongs(listing.nextPageUrl); }
       };
 
       function loadSongs(url) {
         $http.get(url)
           .then(songsRes => songsRes.data)
           .then(songsData => {
-            home.nextPageUrl = songsData.next;
+            listing.nextPageUrl = songsData.next;
             return songsData.results;
           })
           .then(songList => songList.forEach(song => {
-            home.songs.push(song);
+            listing.songs.push(song);
             $http.get(song.url)
               .then(res => Object.assign(song, res.data))
               .then(songData => {
-                home.artists = home.artists.concat(songData.artists);
-                home.albums = home.albums.concat(songData.album_set);
+                listing.artists = listing.artists.concat(songData.artists);
+                listing.albums = listing.albums.concat(songData.album_set);
               })
               .then(() => {
                 const uniqueArtists = [];
-                home.artists.forEach(a => {
+                listing.artists.forEach(a => {
                   if(!uniqueArtists.find(u => a.name === u.name)) { uniqueArtists.push(a); }
                 });
-                home.artists = uniqueArtists;
+                listing.artists = uniqueArtists;
               })
               .then(() => {
                 const uniqueAlbums = [];
-                home.albums.forEach(a => {
+                listing.albums.forEach(a => {
                   if(!uniqueAlbums.find(u => a.name === u.name)) { uniqueAlbums.push(a); }
                 });
-                home.albums = uniqueAlbums;
+                listing.albums = uniqueAlbums;
               })
             }
           ));
