fa9c747e6a9651a86b2209cea1a1ac3b29d8cdc3
Adam Oswalt <email@adamoswalt.com>
2016-09-08 21:30:24 -0500

    single entry artist and album filter dropdowns

--- client/app/home_page_controller.js
+++ client/app/home_page_controller.js
@@ -2,39 +2,55 @@ angular.module("app")
     .controller("HomePageCtrl", function($http, $uibModal, RootFactory) {
       const home = this;
       home.songs = [];
-      home.nextPageUrl = "";
       home.artists = [];
       home.albums = [];
+      home.nextPageUrl = "";
 
-
-      RootFactory.root
-      .then(data => $http.get(data.artist))
-      .then(res => home.artists = res.data.results)
-      .then(s => console.log(s))
-
-      RootFactory.root
-      .then(data => $http.get(data.album))
-      .then(res => home.albums = res.data.results)
-      .then(s => console.log(s))
 
       home.showSongInfo = song => $uibModal.open({
         templateUrl: "app/_song-modal.html",
         controller: "SongInfoModalCtrl",
         controllerAs: "songInfo",
         resolve: { song }
-      })
-      home.more = () => { if(home.nextPageUrl) loadSongs(home.nextPageUrl); }
+      });
+
+      home.more = () => {
+        if(home.nextPageUrl) { loadSongs(home.nextPageUrl); }
+      };
 
       function loadSongs(url) {
         $http.get(url)
           .then(songsRes => songsRes.data)
-          .then(songsData => { home.nextPageUrl = songsData.next; return songsData.results; })
+          .then(songsData => {
+            home.nextPageUrl = songsData.next;
+            return songsData.results;
+          })
           .then(songList => songList.forEach(song =>
             $http.get(song.url)
-              .then(res => {home.songs.push(res.data); return res.data})
+              .then(res => {
+                home.songs.push(res.data);
+                return res.data;
+              })
+              .then(songData => {
+                home.artists = home.artists.concat(songData.artists);
+                home.albums = home.albums.concat(songData.album_set);
+              })
+              .then(() => {
+                const uniqueArtists = [];
+                home.artists.forEach(a => {
+                  if(!uniqueArtists.find(u => a.name === u.name)) { uniqueArtists.push(a); }
+                });
+                home.artists = uniqueArtists;
+              })
+              .then(() => {
+                const uniqueAlbums = [];
+                home.albums.forEach(a => {
+                  if(!uniqueAlbums.find(u => a.name === u.name)) { uniqueAlbums.push(a); }
+                });
+                home.albums = uniqueAlbums;
+              })
           ));
       }
 
       RootFactory.root.then(data => loadSongs(data.song));
-
-    })
+    });
